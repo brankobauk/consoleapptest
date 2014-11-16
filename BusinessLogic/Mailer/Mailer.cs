6baf9d9bf08c6a84de29678f6a61432644081a7e
Brancinho <branko.bauk@gmail.com>
2014-11-16 21:56:29 +0100

    commit

--- BusinessLogic/Mailer/Mailer.cs
+++ BusinessLogic/Mailer/Mailer.cs
@@ -9,27 +9,49 @@ namespace BusinessLogic.Mailer
 {
     public class Mailer : IMailer
     {
+        //public override void SendEmail()
+        //{
+        //    var client = new SmtpClient
+        //    {
+        //        Port = 587,
+        //        Host = "smtp.gmail.com",
+        //        EnableSsl = true,
+        //        Timeout = 10000,
+        //        DeliveryMethod = SmtpDeliveryMethod.Network,
+        //        UseDefaultCredentials = false,
+        //        Credentials = new NetworkCredential("branko@bauk.si", "MajesticView43")
+        //    };
+
+        //    var mm = new MailMessage("branko.bauk@gmail.com", "branko@bauk.si", "test", "test")
+        //    {
+        //        BodyEncoding = Encoding.UTF8,
+        //        DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure
+        //    };
+
+        //    client.Send(mm);
+        // }
+
         public override void SendEmail()
         {
+            // Compose a message
+            var mail = new MailMessage("branko.bauk@gmail.com", "branko@bauk.si")
+            {
+                Subject = "Hello",
+                Body = "Testing some Mailgun awesomness"
+            };
+
+            // Send it!
             var client = new SmtpClient
             {
                 Port = 587,
-                Host = "smtp.gmail.com",
-                EnableSsl = true,
-                Timeout = 10000,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("branko@bauk.si", "MajesticView43")
+                Credentials = new NetworkCredential("postmaster@sandbox9f0a4db277724415ac55aa359f74577b.mailgun.org", "6bebeecd07a65a243eb6216117655fd9"),
+                Host = "sandbox9f0a4db277724415ac55aa359f74577b.mailgun.org"
             };
 
-            var mm = new MailMessage("branko.bauk@gmail.com", "branko@bauk.si", "test", "test")
-            {
-                BodyEncoding = Encoding.UTF8,
-                DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure
-            };
-
-            client.Send(mm);
-         }
+            client.Send(mail);
+        }
 
     }
 }
